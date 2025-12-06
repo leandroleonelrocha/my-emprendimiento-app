31edb59d91bef6224677f89653e7f5f74b8fd6bd
leandro rocha <leandrorocha@MacBook-Air-de-leandro.local>
2025-12-06 17:43:43 -0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import type { ReactNode } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -37,8 +38,7 @@ export const metadata: Metadata = {
   }
 };
 
-
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es">
       <head>
