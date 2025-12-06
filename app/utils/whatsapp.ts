855678919a45cf5680b55c3da06d6b8c3ebc5d66
leandro rocha <leandrorocha@MacBook-Air-de-leandro.local>
2025-12-06 19:07:07 -0300

    update

--- app/utils/whatsapp.ts
+++ app/utils/whatsapp.ts
@@ -1,7 +1,8 @@
 export function generarLinkWhatsApp(nombreProducto: string) {
   const phoneNumber = "5491162174495"; 
   //const phoneNumber = "5491163009079";
-  const websiteURL = "https://my-emprendimiento-app.vercel.app?v=3";
+  const websiteURL = "https://muebles-de-familia.vercel.app?v=3";
+  
 
   const message = `
     Hola, estoy interesado en el producto: ${nombreProducto}.
