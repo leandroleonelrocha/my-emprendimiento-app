6f81dde34292b4fdc88485dbe8b3da976fc1fb69
leandro rocha <leandrorocha@MacBook-Air-de-leandro.local>
2025-12-12 21:31:16 -0300

    update

--- app/data/productos.ts
+++ app/data/productos.ts
@@ -130,8 +130,44 @@ export const productos = [
     ],
     imagenes: [
       "/img/estantes-recibidor-z/1.jpeg",
+      "/img/estantes-recibidor-z/2.jpeg",
     ],
     descripcion:["Realizado en Mdf de 18 milimetros", "Fondo blanco 35 x 20", "Estante largo 25 x 10", "Estante corto 10 x 10", ],
+    //incluye: "Incluye kit de colocacion ( va atornillado )",
+    venta: true,
+    permiteImagenPersonalizada: true
+  },
+  {
+    id: 5,
+    nombre: "Mesa de luz - apego Z",
+    imagen: "/img/mesa-luz/3.png",
+    basePrice: 49000,
+    colores: [
+      { id: "verde", etiqueta: "Verde", hex: "#7ED957" },
+      { id: "rojo", etiqueta: "Rojo", hex: "#FF3131" },
+      { id: "amarillo", etiqueta: "Amarillo", hex: "#F7D84A" },
+      { id: "rosa", etiqueta: "Rosa", hex: "#FF7EB9" },
+    ],
+    medidas: [
+      { id: "30cm", etiqueta: "27", extra: 0 },
+      { id: "50cm", etiqueta: "15", extra: 3000 },
+    ],
+    maderas: [
+      { id: "pino", etiqueta: "Pino Natural", extra: 0 },
+      { id: "roble", etiqueta: "Roble", extra: 4000 },
+      { id: "nogal", etiqueta: "Nogal Oscuro", extra: 6000 },
+    ],
+    ganchos: [
+      { id: 0, etiqueta: "0 ganchos", extra: 0 },
+      { id: 3, etiqueta: "3 ganchos", extra: 1500 },
+      { id: 5, etiqueta: "5 ganchos", extra: 2500 },
+    ],
+    imagenes: [
+     // "/img/mesa-luz/1.jpeg",
+      "/img/mesa-luz/2.jpeg",
+      "/img/mesa-luz/3.png",
+    ],
+    descripcion:["Melamina de 18 milimetros de espesor", "60 de alto, ancho 50 y profundidad de 40"],
     //incluye: "Incluye kit de colocacion ( va atornillado )",
     venta: true,
     permiteImagenPersonalizada: true
